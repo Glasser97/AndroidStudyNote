07bb783085ef1a61531f9a9f8142c05ff4352956
graysonzeng <graysonzeng@futunn.com>
2019-11-07 15:34:49 +0800

    vote button almost finished

--- customView/app/src/main/java/com/example/customview/MainActivity.java
+++ customView/app/src/main/java/com/example/customview/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.customview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView kanhao;
+    TextView bukanhao;
+    VoteButton voteButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,30 @@ public class MainActivity extends AppCompatActivity {
         runText.setRunTextString("这是一个文字跑马灯控件，当文字的长度超过宽度的时候，就会开始跑动，可以设置是否循环，速度，滚动方向，文字颜色，背景颜色等等。并且设定了一个滚动完毕的回调，便于滚动完毕后通知环境");
         runText.startScroll();
 
+        voteButton = (VoteButton) findViewById(R.id.voteButton);
 
+        voteButton.setmLeftNo(5);
+        voteButton.setmRightNo(3);
+        voteButton.setmSlashUnderWidth(100);
+
+        kanhao = (TextView) findViewById(R.id.kanhao);
+        kanhao.setText(voteButton.getmLeftNo()+"");
+
+        bukanhao = (TextView) findViewById(R.id.bukanhao);
+        bukanhao.setText(voteButton.getmRightNo()+"");
+
+        voteButton.setOnVoteClickListener(new VoteButton.VoteClickListener() {
+            @Override
+            public void onClickLeft() {
+                int k = voteButton.getmLeftNo();
+                kanhao.setText(voteButton.setmLeftNo(k+1)+"");
+            }
+
+            @Override
+            public void onClickRight() {
+                int k =voteButton.getmRightNo();
+                bukanhao.setText(voteButton.setmRightNo(k+1)+"");
+            }
+        });
     }
 }
--- customView/app/src/main/java/com/example/customview/VoteButton.java
+++ customView/app/src/main/java/com/example/customview/VoteButton.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -23,25 +29,39 @@ public class VoteButton extends View {
     private int mLeftColor = Color.RED; // TODO: use a default from R.color...
     private int mRightColor = Color.BLUE; // TODO: use a default from R.color...
 
-    private int mLeftNo = 0;
-    private int mRightNo = 0;
-    private float mTextSize = 0; // TODO: use a default from R.dimen...
+    private int mLeftNo;
+    private int mRightNo;
+    private float mTextSize = 12; // TODO: use a default from R.dimen...
     private float mSlashWidth = 5;
-    private Drawable mExampleDrawable;
+    private int middleWall;
+    private Context mContext;
+    private VoteClickListener mVoteClickListener;
 
     private TextPaint mLeftTextPaint;
     private TextPaint mRightTextPaint;
+    private Paint leftColorPaint;
+    private Paint rightColorPaint;
     private float mLeftTextWidth;
     private float mLeftTextHeight;
     private float mRightTextWidth;
     private float mRightTextHeight;
     private float mSlashUnderWidth = 0;
-    private float mXRadius = 30;
-    private float mYRadius = 30;
 
-    //装载按钮控件的区域
-    private RectF rectF = new RectF();
+    //定义绘制的左右Path和圆角
+    private Path leftTriPath;
+    private Path rightTriPath;
+    private CornerPathEffect corner;
 
+    //定义Path绘制圆的扇形区域
+    RectF leftRectF;
+    RectF rightRectF;
+
+    //定义渐变色的shader
+    LinearGradient leftGradient;
+    LinearGradient rightGradient;
+    int[] leftColors;
+    int[] rightColors;
+    float[] colorPositions;
 
 
     public VoteButton(Context context) {
@@ -64,47 +84,49 @@ public class VoteButton extends View {
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.VoteButton, defStyle, 0);
 
-        mLeftString = a.getString(
-                R.styleable.VoteButton_leftString);
-        mRightString = a.getString(
-                R.styleable.VoteButton_rightString);
-        mTextColor = a.getColor(R.styleable.VoteButton_textColor,
-                mTextColor);
-        mLeftColor = a.getColor(R.styleable.VoteButton_leftColor,
-                mLeftColor);
-        mRightColor = a.getColor(R.styleable.VoteButton_rightColor,
-                mRightColor);
-        mSlashUnderWidth = a.getDimension(R.styleable.VoteButton_slashUnderWidth,
-                mSlashUnderWidth);
+        mLeftString = a.getString(R.styleable.VoteButton_leftString);
+        mRightString = a.getString(R.styleable.VoteButton_rightString);
+        mTextColor = a.getColor(R.styleable.VoteButton_textColor, mTextColor);
+        mLeftColor = a.getColor(R.styleable.VoteButton_leftColor, mLeftColor);
+        mRightColor = a.getColor(R.styleable.VoteButton_rightColor, mRightColor);
+        mSlashUnderWidth = a.getDimension(R.styleable.VoteButton_slashUnderWidth, mSlashUnderWidth);
         mSlashWidth = a.getDimension(R.styleable.VoteButton_slashWidth,mSlashWidth);
-        mXRadius = a.getDimension(R.styleable.VoteButton_XRadius,mXRadius);
-        mYRadius = a.getDimension(R.styleable.VoteButton_YRadius, mYRadius);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mTextSize = a.getDimension(
-                R.styleable.VoteButton_textSize,
-                mTextSize);
-        rectF.left = 0;
-        rectF.top = 0;
-//        rectF.right = width;
-//        rectF.
-
-
-//        if (a.hasValue(R.styleable.VoteButton_exampleDrawable)) {
-//            mExampleDrawable = a.getDrawable(
-//                    R.styleable.VoteButton_exampleDrawable);
-//            mExampleDrawable.setCallback(this);
-//        }
+        mTextSize = a.getDimension(R.styleable.VoteButton_textSize, mTextSize);
 
         a.recycle();
 
-        // Set up a default TextPaint object
+        // 初始化绘图Path，以及圆角
+        leftTriPath = new Path();
+        rightTriPath = new Path();
+        corner = new CornerPathEffect(10);
+        leftRectF = new RectF();
+        rightRectF = new RectF();
+
+        //初始化渐变颜色数组和渐变位置数组
+
+        leftColors = new int[]{mLeftColor, brighterColor(mLeftColor,1.5f)};
+        rightColors = new int[]{mRightColor, brighterColor(mRightColor,1.5f)};
+        colorPositions = new float[]{0.3f,0.8f};
+
+
+        // 初始化画笔
         mLeftTextPaint = new TextPaint();
         mLeftTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
         mLeftTextPaint.setTextAlign(Paint.Align.LEFT);
         mRightTextPaint = new TextPaint();
         mRightTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
         mRightTextPaint.setTextAlign(Paint.Align.RIGHT);
+        leftColorPaint = new Paint();
+        leftColorPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        leftColorPaint.setColor(mLeftColor);
+        leftColorPaint.setStyle(Paint.Style.FILL);
+        leftColorPaint.setPathEffect(corner);
+        rightColorPaint = new Paint();
+        rightColorPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        rightColorPaint.setColor(mRightColor);
+        rightColorPaint.setStyle(Paint.Style.FILL);
+        rightColorPaint.setPathEffect(corner);
+
 
         // Update TextPaint and text measurements from attributes
         invalidateTextPaintAndMeasurements();
@@ -124,28 +146,46 @@ public class VoteButton extends View {
 
     }
 
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
-//        height = measuredHeight(heightMeasureSpec);
-//        width = measureWidth(widthMeasureSpec);
-//        setMeasureDimension(width, height);
-//    }
-//
-//    private int measureWidth(int widthMeasureSpec) {
-//        int result;
-//        int specMode = MeasureSpec.getMode(widthMeasureSpec);
-//        int specSize = MeasureSpec.getSize(widthMeasureSpec);
-//        if (specMode == MeasureSpec.EXACTLY) {
-//            result = specSize;
-//        } else {//这是wrap的模式，给一个固定大小
-////            result = (int) getContext().getResources().getDimension(R.dimen.dp_150);
-//            result = buttonString.length() * mTextSize + height * 5 / 3;
-//            if (specMode == MeasureSpec.AT_MOST) {
-//                result = Math.min(result, specSize);
-//            }
-//        }
-//        return result;
-//    }
+    //测量控件的宽高
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        height = measuredHeight(heightMeasureSpec);
+        width = measureWidth(widthMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+
+    /**
+     * 在除了Exactly的模式下，认为空间的宽度为填满父控件的宽度
+     * 高度如果为wrap，模式则为文字的高度加上35dp的高度
+     * @param widthMeasureSpec
+     * @return
+     */
+    private int measureWidth(int widthMeasureSpec) {
+        int result;
+        int specMode = MeasureSpec.getMode(widthMeasureSpec);
+        int specSize = MeasureSpec.getSize(widthMeasureSpec);
+        result = specSize;
+        return result;
+    }
+
+    private int measuredHeight(int heightMeasureSpec){
+        int result;
+        int specMode = MeasureSpec.getMode(heightMeasureSpec);
+        int specSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = (int)(mTextSize + getContext().getResources().getDimension(R.dimen.dp_20));
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    public void setOnVoteClickListener(VoteClickListener voteClickListener){
+        this.mVoteClickListener = voteClickListener;
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -161,54 +201,129 @@ public class VoteButton extends View {
         int contentWidth = getWidth() - paddingLeft - paddingRight;
         int contentHeight = getHeight() - paddingTop - paddingBottom;
 
-        // Draw the text.
-        canvas.drawText(mLeftString,
-                paddingLeft + (contentWidth - mLeftTextWidth) / 2,
-                paddingTop + (contentHeight + mLeftTextHeight) / 2,
-                mLeftTextPaint);
-        //canvas.drawRoundRect(rectF,);
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
+        int halfH = contentHeight/2;
+        int rectWidth = contentWidth-contentHeight;
+        int halfSlash = (int)(mSlashUnderWidth/2 + mSlashWidth/2);
+//        canvas.drawCircle(paddingLeft+halfH,halfH+paddingTop,halfH,leftColorPaint);
+//        canvas.drawCircle(getWidth()-paddingRight-halfH,paddingTop+halfH,halfH,rightColorPaint);
+
+        //计算左边所占百分比
+        int leftRectWidth = (int)(computeLength(mLeftNo,mRightNo) * rectWidth);
+        //设置点击事件的中间分割线
+        middleWall = paddingLeft+halfH+leftRectWidth;
+
+        if(leftGradient == null){
+            leftGradient = new LinearGradient(paddingLeft,paddingTop+halfH,
+                    paddingLeft+halfH+leftRectWidth-halfSlash+mSlashUnderWidth, paddingTop,
+                    leftColors,colorPositions, Shader.TileMode.CLAMP);
         }
+        if(rightGradient == null){
+            rightGradient = new LinearGradient(getWidth()-paddingRight,paddingTop+halfH,
+                    paddingLeft+halfH+leftRectWidth+halfSlash-mSlashUnderWidth,paddingTop+contentHeight,
+                    rightColors,colorPositions, Shader.TileMode.CLAMP);
+        }
+
+        //使用path画出左边的按钮
+        leftRectF.set(paddingLeft,paddingTop,paddingLeft+contentHeight,paddingTop+contentHeight);
+        leftTriPath.moveTo(paddingLeft+halfH+leftRectWidth-halfSlash,paddingTop);
+        leftTriPath.lineTo(paddingLeft+halfH,paddingTop);
+        leftTriPath.arcTo(leftRectF,270 ,-180,false);
+        leftTriPath.lineTo(paddingLeft+halfH+leftRectWidth-halfSlash,paddingTop+contentHeight);
+        leftTriPath.lineTo(paddingLeft+halfH+leftRectWidth-halfSlash+mSlashUnderWidth,paddingTop);
+        leftTriPath.close();
+        leftColorPaint.setShader(leftGradient);
+        canvas.drawPath(leftTriPath,leftColorPaint);
+
+        //使用path画出右边的按钮
+        rightRectF.set(getWidth()-paddingRight-contentHeight,paddingTop,getWidth()-paddingRight,paddingTop+contentHeight);
+        rightTriPath.moveTo(paddingLeft+halfH+leftRectWidth+halfSlash,paddingTop+contentHeight);
+        rightTriPath.lineTo(getWidth()-paddingRight-halfH,paddingTop+contentHeight);
+        rightTriPath.arcTo(rightRectF,90,-180,false);
+        rightTriPath.lineTo(paddingLeft+halfH+leftRectWidth+halfSlash, paddingTop);
+        rightTriPath.lineTo(paddingLeft+halfH+leftRectWidth+halfSlash-mSlashUnderWidth,paddingTop+contentHeight);
+        rightTriPath.close();
+        rightColorPaint.setShader(rightGradient);
+        canvas.drawPath(rightTriPath,rightColorPaint);
+
+
+        //绘制文字
+        canvas.drawText(mLeftString, paddingLeft + halfH, paddingTop + halfH + mLeftTextHeight+6, mLeftTextPaint);
+        canvas.drawText(mRightString,getWidth()-paddingRight-halfH,paddingTop + halfH + mLeftTextHeight+6,mRightTextPaint);
+        // Draw the example drawable on top of the text.
+//        if (mExampleDrawable != null) {
+//            mExampleDrawable.setBounds(paddingLeft, paddingTop,
+//                    paddingLeft + contentWidth, paddingTop + contentHeight);
+//            mExampleDrawable.draw(canvas);
+//        }
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                int x = (int)event.getX();
+                if (x < middleWall) {
+                    mVoteClickListener.onClickLeft();
+                } else if(x > middleWall){
+                    mVoteClickListener.onClickRight();
+                }
+                //invalidate();
+                break;
+        }
+        return true;
+    }
+
+    private int brighterColor(int color, float brighterK){
+        int alpha = (color & 0xff000000);
+        int red = (int)(((color & 0x00ff0000) >> 16) * brighterK);
+        int green = (int)(((color & 0x0000ff00) >> 8) * brighterK);
+        int blue = (int)((color & 0x000000ff) * brighterK);
+        red = red > 255 ? 255:red;
+        green = green > 255 ? 255:green;
+        blue = blue > 255 ? 255:blue;
+        return alpha + (red << 16) + (green << 8) + blue;
+    }
+    private double computeLength(int leftNo, int rightNo){
+        double half = 1.0/2.0;
+        if(leftNo == 0 || rightNo == 0){
+            return half;
+        }else{
+            return (double)leftNo/(double)(leftNo+rightNo);
+        }
+    }
 
     public int getmLeftNo() {
         return mLeftNo;
     }
 
-    public void setmLeftNo(int mLeftNo) {
+    public int setmLeftNo(int mLeftNo) {
         this.mLeftNo = mLeftNo;
+        return this.mLeftNo;
     }
 
     public int getmRightNo() {
         return mRightNo;
     }
 
-    public void setmRightNo(int mRightNo) {
+    public int setmRightNo(int mRightNo) {
         this.mRightNo = mRightNo;
+        return this.mRightNo;
     }
 
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
+    public void setmSlashUnderWidth(float mSlashUnderWidth) {
+        this.mSlashUnderWidth = mSlashUnderWidth;
     }
 
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
+    public float getmSlashUnderWidth() {
+        return mSlashUnderWidth;
+    }
+
+    public interface VoteClickListener{
+        void onClickLeft();
+        void onClickRight();
     }
 }
